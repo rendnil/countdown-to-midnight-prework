e845b16546a834ac3f95e28576a28e2caf14bf6e
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-12 19:52:41 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -18,3 +18,24 @@ end
   
   
 end
+
+
+def countdown_with_sleep(integer)
+  
+    while integer >0
+  
+  puts "#{integer} SECOND(S)!"
+  
+  integer -=1
+  
+  
+end
+  
+
+
+
+ "HAPPY NEW YEAR!" 
+  
+  
+end
+
